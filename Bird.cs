b22ab648ba98fca245937b3c7c7d3582bc444efd
thetubeguy <59772776+thetubeguy@users.noreply.github.com>
2022-05-11 09:50:27 +0100

    Add project files.

---
+++ Bird.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace introduction_to_oop_project
+{
+    public class Bird
+    {
+        public string Name { get; private set; }
+        public string Color { get; private set; }
+
+        public Bird(string name, string color)
+        {
+            Name = name;
+            Color = color;
+        }
+
+        public void Speak()
+        {
+            Console.WriteLine($"My name is {Name} and I am a {Color} bird.");
+        }
+
+        public void Fly()
+        {
+            Console.WriteLine($"I'm {Name} and I can fly high in the blue sky!");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace introduction_to_oop_project
+{
+    public class Program 
+    {
+        static void Main(string[] args)
+        {
+
+            Bird myBird = new Bird("Tweety", "Yellow");
+            myBird.Speak();
+            myBird.Fly();
+
+        }
+    }
+}
